24b5493041360cf8ca99743a7f5e5e3d4977bd07
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2019-01-16 10:06:07 +0900

    シングルプレイメニューの反映

--- Assets/NotBuyoTeto/Scripts/Ingame/SinglePlay/MenuManager.cs
+++ Assets/NotBuyoTeto/Scripts/Ingame/SinglePlay/MenuManager.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using NotBuyoTeto.SceneManagement;
 using NotBuyoTeto.Constants;
+using NotBuyoTeto.UI;
 
 namespace NotBuyoTeto.Ingame.SinglePlay {
     public class MenuManager : MonoBehaviour {
+        [SerializeField]
+        private SelectableToggler toggler;
+
         public void Update() {
             if (Input.GetKeyDown(KeyCode.Escape)) {
                 SceneController.Instance.LoadScene(SceneName.Title, SceneTransition.Duration);
             }
+        }
+
+        public void OnPressedTetrinButton() {
+            toggler.ToggleAll();
+            SceneController.Instance.LoadScene(SceneName.MarathonMode, SceneTransition.Duration);
+        }
+
+        public void OnPressedBuyoBuyoButton() {
+            toggler.ToggleAll();
+            SceneController.Instance.LoadScene(SceneName.TokotonMode, SceneTransition.Duration);
         }
     }
 }
