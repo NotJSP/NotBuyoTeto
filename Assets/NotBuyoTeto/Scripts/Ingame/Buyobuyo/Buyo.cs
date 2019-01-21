e7e5dd5eade5ba4912a67a94eaffd0c3ca66e872
kbc16a09 <kbc16a09@stu.kawahara.ac.jp>
2019-01-21 12:02:23 +0900

    ぶよにGlowEffectを追加

--- Assets/NotBuyoTeto/Scripts/Ingame/Buyobuyo/Buyo.cs
+++ Assets/NotBuyoTeto/Scripts/Ingame/Buyobuyo/Buyo.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using System;
+using SpriteGlow;
 
 namespace NotBuyoTeto.Ingame.Buyobuyo {
-    [RequireComponent(typeof(SpriteRenderer), typeof(Collider2D))]
+    [RequireComponent(typeof(SpriteRenderer), typeof(Collider2D), typeof(SpriteGlowEffect))]
     public class Buyo : MonoBehaviour, IEqualityComparer<Buyo> {
         [SerializeField]
         private BuyoType type;
@@ -13,19 +14,32 @@ namespace NotBuyoTeto.Ingame.Buyobuyo {
 
         private HashSet<Buyo> chainObjects = new HashSet<Buyo>();
         private ParticleSystem BuyoDeleteEffect;
+        private SpriteGlowEffect glowEffect;
 
         public event EventHandler<Vector2> DeleteBuyo;
 
         private void Awake() {
             type = GetComponent<Buyo>().Type;
             BuyoDeleteEffect = gameObject.GetComponentInChildren<ParticleSystem>();
+            glowEffect = GetComponent<SpriteGlowEffect>();
+            glowEffect.GlowBrightness = 0.0f;
+            glowEffect.OutlineWidth = 0;
         }
 
         // Update is called once per frame
         void Update() {
-            if (GetChainCount(null) >= 4) {
+            int chainCount = GetChainCount(null);
+            if (chainCount >= 4) {
                 DestroyChain(null);
                 DeleteBuyo?.Invoke(this, transform.position);
+            }
+            if (chainCount < 3) {
+                glowEffect.GlowBrightness = 0.0f;
+                glowEffect.OutlineWidth = 0;
+            }
+            if (chainCount >= 3) {
+                glowEffect.GlowBrightness = 1.25f;
+                glowEffect.OutlineWidth = 12;
             }
         }
 
@@ -71,7 +85,7 @@ namespace NotBuyoTeto.Ingame.Buyobuyo {
             var chainObj = collision.gameObject.GetComponent<Buyo>();
 
             if (chainObj.type == this.type) {
-                chainObjects.Add(chainObj);
+                chainObjects.Add(chainObj);                
             }
         }
 
