8cf2c31577b3b5ad5606dcf7563b42d98d975e9d
kbc16a09 <kbc16a09@stu.kawahara.ac.jp>
2019-01-30 14:39:38 +0900

    不要ファイルの削除

--- Assets/NotBuyoTeto/Scripts/Ingame/Buyobuyo/Buyo.cs
+++ Assets/NotBuyoTeto/Scripts/Ingame/Buyobuyo/Buyo.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using System;
-using SpriteGlow;
 
 namespace NotBuyoTeto.Ingame.Buyobuyo {
     [RequireComponent(typeof(SpriteRenderer), typeof(Collider2D))]
