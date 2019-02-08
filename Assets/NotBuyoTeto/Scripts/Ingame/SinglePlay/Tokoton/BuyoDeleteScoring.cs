0e041bb2a961eb663d238f500bcce22ae0b72212
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2019-02-08 15:30:27 +0900

    スコア計算式の変更

--- Assets/NotBuyoTeto/Scripts/Ingame/SinglePlay/Tokoton/BuyoDeleteScoring.cs
+++ Assets/NotBuyoTeto/Scripts/Ingame/SinglePlay/Tokoton/BuyoDeleteScoring.cs
@@ -8,9 +8,13 @@ namespace NotBuyoTeto.Ingame.SinglePlay.Tokoton {
         [SerializeField]
         private ScoreManager score;
         
-        public void buyoDeleteScoring(int combo, int level) {
+        public void buyoDeleteScoring(int level, DeleteBuyoInfo info) {
             var baseScore = 200.0f;
-            var amount = baseScore + (combo * level * 50);
+            var levelsAmount = 1.0f + 0.15f * Mathf.Pow(level, 1.05F);
+            var combosAmount = 1.5 * Mathf.Pow(info.ComboCount, 2f);
+            var objectsAmount = Mathf.Pow((info.ObjectCount - 3), 0.25f);
+            var amount = baseScore * levelsAmount * combosAmount * objectsAmount;
+            Debug.Log("Score: " + amount);
             score.Increase((int)amount);
         }
     }
